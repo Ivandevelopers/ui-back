7293cf2d71846b776ca5e5503ca62e62851c7068
AndrewKrava <kravecandrew@gmail.com>
2024-05-07 11:26:39 +0300

    refactor read parametrs for autopilot

--- src/autopilot_params.cpp
+++ src/autopilot_params.cpp
@@ -9,6 +9,48 @@
 
 #define DEBUG
 
+class ParamBase
+{
+public:
+  virtual ~ParamBase() {}
+  virtual std::string getName() const = 0;
+  virtual float getValue() const = 0;
+};
+
+class FloatParam : public ParamBase
+{
+public:
+  FloatParam(const mavsdk::Param::FloatParam &param) : name(param.name), value(param.value) {}
+
+  std::string getName() const override { return name; }
+  float getValue() const override { return value; }
+
+  // Add other members, if needed
+private:
+  std::string name;
+  float value;
+};
+
+// Define IntParam class implementing ParamBase
+class IntParam : public ParamBase
+{
+public:
+  IntParam(const mavsdk::Param::IntParam &param) : name(param.name), value(param.value) {}
+
+  std::string getName() const override { return name; }
+  float getValue() const override { return value; }
+
+  // Add other members, if needed
+private:
+  std::string name;
+  int value;
+};
+
+bool compareParams(const std::unique_ptr<ParamBase> &param1, const std::unique_ptr<ParamBase> &param2)
+{
+  return param1->getName() < param2->getName();
+}
+
 void readParams(const std::string &filename,
                 const mavsdk::Param::AllParams &all_params)
 {
@@ -20,21 +62,26 @@ void readParams(const std::string &filename,
     return;
   }
 
-  for (const auto &float_param : all_params.float_params)
+  // Combine float_params and int_params into a single vector of ParamBase pointers
+  std::vector<std::unique_ptr<ParamBase>> all_sorted_params;
+  for (const mavsdk::Param::FloatParam &float_param : all_params.float_params)
   {
-    outputFile << float_param.name << " " << float_param.value << std::endl;
-
-#if defined(DEBUG)
-    std::cout << float_param.name << " " << float_param.value << std::endl;
-#endif
+    all_sorted_params.push_back(std::make_unique<FloatParam>(float_param));
+  }
+  for (const mavsdk::Param::IntParam &int_param : all_params.int_params)
+  {
+    all_sorted_params.push_back(std::make_unique<IntParam>(int_param));
   }
 
-  for (const auto &int_param : all_params.int_params)
-  {
-    outputFile << int_param.name << " " << int_param.value << std::endl;
+  // Sort the combined vector based on parameter names
+  std::sort(all_sorted_params.begin(), all_sorted_params.end(), compareParams);
 
+  // Write the sorted parameters to the file
+  for (const auto &param : all_sorted_params)
+  {
+    outputFile << param->getName() << "," << param->getValue() << std::endl;
 #if defined(DEBUG)
-    std::cout << int_param.name << " " << int_param.value << std::endl;
+    std::cout << param->getName() << " " << param->getValue() << std::endl;
 #endif
   }
 
@@ -69,14 +116,14 @@ mavsdk::Param::AllParams writeParams(const std::string &filename)
       {
         all_params.int_params.push_back(
             {paramName, static_cast<int32_t>(paramValue)});
-        std::cout << paramName << " " << static_cast<int32_t>(paramValue)
+        std::cout << paramName << "," << static_cast<int32_t>(paramValue)
                   << std::endl;
       }
       else
       {
         all_params.float_params.push_back(
             {paramName, static_cast<float>(paramValue)});
-        std::cout << paramName << " " << paramValue << std::endl;
+        std::cout << paramName << "," << paramValue << std::endl;
       }
     }
     else
